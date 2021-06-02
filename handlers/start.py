0917265c6e6a08245b8449bbb292578213ad71de
natasha026 <81474131+natasha026@users.noreply.github.com>
2021-06-02 15:51:20 +0530

    Update and rename start to start.py

---
+++ handlers/start.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+
+
+
+
+@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+async def start(_, message: Message):
+    await message.reply_text(
+        #f"""<b>Hey {message.from_user.first_name}!</b>
+#I can play music in your Telegram's voice chat!
+#Use the buttons below to know more about how to use me.""",
+        #reply_markup=InlineKeyboardMarkup(
+            #[ 
+                #[
+                    #InlineKeyboardButton(
+                       # "📞 Contact", url="https://t.me/T_r_oy"
+                   # ),
+                   # InlineKeyboardButton(
+                     #   "🛸 Channel", url="https://t.me/Wandabetaxbot"
+                  #  )
+               # ],                    
+              #  [                    
+                  #  InlineKeyboardButton(
+                    #    "📜 User Manual", url="https://telegra.ph/MusicXstreamingbot-05-12"
+                  #  )                    
+            #    ]
+          #  ]
+     #   ),
+   #  disable_web_page_preview=True
+ #   )
+
+#@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+#async def gstart(_, message: Message):
+  #    await message.reply_text("""**🔴 Music player is online**""",
+  #    reply_markup=InlineKeyboardMarkup(
+     #       [
+     #           [
+           #         InlineKeyboardButton(
+                 #       "🎙️ Support Group 🎙️", url="https://t.me/Wandabetaxbot")
+              #  ]
+        #    ]
+       # )
+#   )
+
